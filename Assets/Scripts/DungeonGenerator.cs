3ec48779b5d1118ac6a16dcf3557b91f728a306b
Lorelei Wright <wrighl@rpi.edu>
2018-10-10 15:10:13 -0400

    Basic Room placement

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour 
+{
+
+	[SerializeField] int width;
+	[SerializeField] int height;
+	[SerializeField] float room_size;
+	[SerializeField] float margin;
+
+	[SerializeField] GameObject room_prefab;
+
+	void Start() { GenerateDungeon(); }
+
+	void GenerateDungeon()
+	{
+		// Place Rooms
+		for (int i = 0; i < width; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				Instantiate(room_prefab, (Vector3.right * i * (room_size + margin)) +
+					(Vector3.forward * j * (room_size + margin)), Quaternion.identity);
+			}
+		}
+
+		// Place Hallways
+
+		// Place Enemies
+
+		// Place "Treasure"
+	}
+
+	
+
+}
