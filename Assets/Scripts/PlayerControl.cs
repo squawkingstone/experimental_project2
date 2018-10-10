ef50d251856f81ae6b2b339fa1a36c2967cb30f2
Max Sichel <msik314@gmail.com>
2018-10-10 12:45:41 -0400

    Add Unity Project

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private string forwardAxis;
+    [SerializeField] private string sidewaysAxis;
+    [SerializeField] private string jumpAxis;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float groundAccel;
+    [SerializeField] private float airAccel;
+    [SerializeField] private float angularSpeed;
+    [SerializeField] private float maxWalkAngle;
+    [SerializeField] private float angularTarget;
+    [SerializeField] private float jumpHeight;
+
+    private Rigidbody rb;
+    private Vector3 groundedNormal;
+    private float cosWalkAngle;
+    private Vector3 movementAxisVector;
+    private Quaternion cameraForwardRotation;
+    private int frameCounter = 0;
+    private float jumpForce;
+    private bool jump = false;
+    PlayerTrackerCam cam;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        groundedNormal = Vector3.zero;
+        cosWalkAngle = Mathf.Cos(Mathf.Deg2Rad * maxWalkAngle);
+        jumpForce = Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * jumpHeight);
+        jump = false;
+        movementAxisVector = Vector3.zero;
+        cameraForwardRotation = Quaternion.identity;
+        cam = FindObjectOfType<PlayerTrackerCam>();
+    }
+
+    void Update()
+    {
+        movementAxisVector += new Vector3 (Input.GetAxisRaw(sidewaysAxis), 0, Input.GetAxisRaw(forwardAxis));
+        jump |= Input.GetButtonDown(jumpAxis);
+        cameraForwardRotation = cam.GetForwardRotator();
+        ++frameCounter;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movementNormal = Vector3.up;
+        Vector3 vel = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        bool grounded = false;
+
+        if(groundedNormal.sqrMagnitude > 0)
+        {
+            movementNormal = groundedNormal.normalized;
+            grounded = true;
+        }
+
+        if(grounded && jump)
+        {
+            rb.AddForce(movementNormal * jumpForce * rb.mass, ForceMode.Impulse);
+        }
+
+        Vector3 movement = vel;
+        if(frameCounter > 0)
+        {
+            movement = cameraForwardRotation * movementAxisVector/frameCounter;
+
+        }
+        float moveLength = movement.magnitude;
+
+        if(moveLength > 0)
+        {
+            movement = (movement - Vector3.Dot(movementNormal, movement) * movementNormal).normalized;
+            movement *= moveLength * movementSpeed;
+        }
+
+        float accel = grounded ? groundAccel : airAccel;
+        float deltaAccel = accel * Time.fixedDeltaTime;
+
+        Vector3 velDiff = movement - vel;
+        
+        if(velDiff.sqrMagnitude < deltaAccel * deltaAccel)
+        {
+            rb.AddForce(velDiff * rb.mass, ForceMode.Impulse);
+        }
+        else
+        {
+            velDiff = velDiff.normalized;
+            rb.AddForce(velDiff * accel * rb.mass);
+        }
+        
+        frameCounter = 0;
+        jump = false;
+        movementAxisVector = Vector3.zero;
+        groundedNormal = Vector3.zero;
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        foreach(ContactPoint contact in col.contacts)
+        {
+            float cosNormalAngle = Vector3.Dot(contact.normal, Vector3.up);
+
+            if(cosNormalAngle > cosWalkAngle)
+            {
+                groundedNormal += contact.normal;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerTrackerCam.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrackerCam : MonoBehaviour
+{
+    [SerializeField] private string zoomAxis;
+    [SerializeField] private string panAxis;
+    [SerializeField] private Vector3 trackOffset;
+    [SerializeField] private Vector3 pathClosePoint;
+    [SerializeField] private Vector3 pathFarPoint;
+    [SerializeField] private AnimationCurve path;
+    [SerializeField] private Transform trackedObject;
+    [SerializeField] private Vector2 sensitivity;
+    [SerializeField] private bool invertX;
+    [SerializeField] private bool invertY;
+    [SerializeField][Range(0.0f, 1.0f)] private float responsiveness;
+
+    private float pathDistance;
+    private Quaternion forwardRotation;
+    private Vector2 inversions;
+
+    void Awake()
+    {
+        forwardRotation = Quaternion.LookRotation(new Vector3(trackedObject.position.x - transform.position.x, transform.position.y, trackedObject.position.z - transform.position.z));
+        transform.rotation = forwardRotation;
+        pathDistance = 0.5f;
+        Move(1.0f);
+        inversions = Vector2.one;
+    }
+
+    void Update()
+    {
+        inversions.x = invertX ? -1 : 1;
+        inversions.y = invertY ? -1 : 1;
+        Move(responsiveness);
+    }
+
+    void Move(float coverDistance)
+    {
+        forwardRotation *= Quaternion.Euler(0, inversions.x * sensitivity.x * Input.GetAxisRaw(panAxis) * Time.deltaTime, 0); 
+        Vector3 trackedPoint =  trackedObject.position + forwardRotation * trackOffset;
+        Vector3 closePoint = forwardRotation * pathClosePoint + trackedPoint;
+        Vector3 farPoint = forwardRotation * pathFarPoint + trackedPoint;
+        pathDistance = Mathf.Clamp01(pathDistance + Input.GetAxisRaw(zoomAxis) * inversions.y * sensitivity.y * Time.deltaTime);
+        Vector3 position = Vector3.Lerp(closePoint, farPoint, pathDistance);
+        position.y = Mathf.Lerp(closePoint.y, farPoint.y, path.Evaluate(pathDistance));
+        transform.position = Vector3.Lerp(transform.position, position, coverDistance);
+        transform.rotation = Quaternion.LookRotation(trackedPoint - transform.position, Vector3.up);
+    }
+
+    public Quaternion GetForwardRotator()
+    {
+        return forwardRotation;
+    }
+}
