4c8989422d03fe0f64dcac3aae1e61161ff772e6
Lorelei Wright <wrighl@rpi.edu>
2018-10-17 13:10:40 -0400

    Added animation controller script

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour {
+
+	[SerializeField] Animator anim;
+	[SerializeField] Rigidbody rb;
+	
+	void FixedUpdate()
+	{
+		Debug.Log(rb.velocity.magnitude);
+		if (rb.velocity.magnitude > 0.01f && !anim.GetBool("IsRunning"))
+		{
+			anim.SetBool("IsRunning", true);
+		}
+		else if (rb.velocity.magnitude <= 0.01f && anim.GetBool("IsRunning"))
+		{
+			anim.SetBool("IsRunning", false);
+		}
+	}
+}
