1a170d6b9534a37248032c8dba846664982471ed
Max Sichel <msik314@gmail.com>
2018-10-28 02:34:42 -0400

    Implement boss hand IK with teleporting models in incorrect poses

---
+++ Assets/Scripts/IkHandControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))] 
+public class IkHandControl : MonoBehaviour
+{
+    [SerializeField] public Transform trackedObject;
+    protected Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        if(animator != null)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKPosition(AvatarIKGoal.LeftHand, trackedObject.position);
+        }
+    }
+}
