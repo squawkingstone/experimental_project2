38554b59e7e73bdcad99bfc6ac687de5b52ffa05
Lorelei Wright <wrighl@rpi.edu>
2018-10-31 01:20:22 -0400

    Added title and survey scenes

---
+++ Assets/Scripts/OpenSurvey.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenSurvey : MonoBehaviour {
+
+	[SerializeField] string url;
+
+	void Start()
+	{
+		Button b = GetComponent<Button>();
+		b.onClick.AddListener(() => {Application.OpenURL(url);});
+	}
+
+}
