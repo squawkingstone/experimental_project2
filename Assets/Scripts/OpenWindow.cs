0baf44868eeaa9d865c4162a6a537397c66de77c
Max Sichel <msik314@gmail.com>
2018-10-31 02:02:49 -0400

    Add shopkeeper sound

--- Assets/Scripts/OpenWindow.cs
+++ Assets/Scripts/OpenWindow.cs
@@ -2,27 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class OpenWindow : MonoBehaviour {
 
 	[SerializeField] GameObject m_Window;
+    [SerializeField] private AudioClip soundClip;
 
 	CloseWindow m_CloseWindow;
 
 	GameObject player;
 	float m_DistanceToPlayer = 4f;
+    private AudioSource source;
 
 	void Start()
 	{
 		m_CloseWindow = m_Window.GetComponentInChildren<CloseWindow>();
 		m_Window.SetActive(false);
 		player = GameObject.FindGameObjectWithTag("Player");
+        source = GetComponent<AudioSource>();
 	}
 
 	void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.E) 
 			&& Vector3.Distance(player.transform.position, transform.position) <= m_DistanceToPlayer)
-			m_Window.SetActive(true);
+            {
+			    m_Window.SetActive(true);
+                source.clip = soundClip;
+                source.Play();
+            }
 
 		if (m_Window.activeInHierarchy && Vector3.Distance(player.transform.position, transform.position) > m_DistanceToPlayer)
 			m_Window.SetActive(false);
